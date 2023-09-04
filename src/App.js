aa63cee9b95102a0898783339ed8ad1c25c85b9e
Nathan Van Zandt <user@Users-MacBook-Pro.local>
2023-09-04 14:19:07 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React,{useEffect, useState} from 'react';
+import Recipe from'./Recipe';
+import './App.css';
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsCupStraw } from "react-icons/bs";
+
+
+function App() {
+
+  const APP_ID = 'be4f50c9'; 
+  const APP_KEY = '05c7563e42cd2ea9bf0a1e9018f2b1ec';
+ 
+  // States
+  // Creates state, function to alter state, and inital value of state
+  const[recipes, setRecipes] = useState([]); // List of hits from api fetching
+  const[search, setSearch] = useState(''); // Search input string
+  const[query, setQuery] = useState('');
+
+  // Effects
+  // Runs after page renders or when [] is updated
+  useEffect(()=> { 
+    getRecipes();
+  }, [query]);
+
+  /*  
+  * Makes asyncronous call, while awaiting the data to be fetched from the api,
+  * the rest of the code after the call is still executed.
+  */
+  const getRecipes = async () => { 
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}%20smoothie&app_id=${APP_ID}&app_key=${APP_KEY}&dishType=Drinks`);
+    const data = await response.json(); 
+    // Show all recipes in console
+    console.log(data.hits);
+    // Store the recipes in the "recipes" state
+    setRecipes(data.hits);
+  }
+
+  // Update the "search" state with the input value
+  const updateSearch = e => { 
+    setSearch(e.target.value);
+  }
+
+  // Update the "query" state with the "search" state 
+  const getSearch = e => { 
+    // Prevent page refresh
+    e.preventDefault();
+    // Get rid of commas
+    setQuery(search.replace(/,/g, ""));
+    setSearch('');
+  }
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1 className="title"><BsCupStraw size={60} style={{marginBottom: 10}}/>Smoothie Search</h1>
+        <p className="directions">Enter the ingredients you want in your smoothie! (e.g.: strawberry, banana, ice) <br/> Note: There is a limit of 10 searches/min</p>
+        <form onSubmit={getSearch} className="search-form"> 
+          <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+          <button className="search-button" type="submit">
+            <AiOutlineSearch size={20} />
+          </button>
+        </form>
+      </div>
+      <div className="recipes">
+        {recipes.map(recipe =>(
+          /*  
+          * Iterate through the "recipes" array and for each recipe, show the "Recipe" component
+          * and create props with api data for each Recipe component.
+          */
+          <Recipe 
+            key={recipe.recipe.label}
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+      <footer className="footer">Thanks for visiting my page!</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import style from './recipe.module.css';
+
+// Create recipe component
+// Pass in props
+const recipe = ({title, calories, image, ingredients}) => { 
+  return ( 
+    <div className={style.recipe}>
+      <h1>{title}</h1> 
+      <img className={style.image} src={image} />
+      <p>{Math.round(calories)} Calories</p>
+      <ul>
+        {ingredients.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default recipe;
